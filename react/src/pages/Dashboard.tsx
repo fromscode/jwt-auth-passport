60c9aa8ccbd947c89fc9b5f2819ec49dd0cc2d60
fromscode <chakrabortyshubham00@gmail.com>
2026-03-31 17:01:50 +0530

    Create dashboard

--- react/src/pages/Dashboard.tsx
+++ react/src/pages/Dashboard.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
 export default function Dashboard() {
+    const [userData, setUserData] = useState<null | any>(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function getUserData() {
+            try {
+                const response = await fetch('http://localhost:3000/dashboard', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                    }
+                })
+
+                if (response.status == 401) navigate('/login');
+                else if (response.status == 200) {
+                    const body = await response.json();
+                    setUserData(body);
+                }
+            }
+            catch (err) {
+                console.error(err);
+                navigate('/')
+            }
+        }
+
+        getUserData();
+    }, []);
+
+    if (!userData) return <>Loading...</>
+
     return <>
-        <h1>This is dashboard</h1>
+        <h1>Welcome to dashboard {userData.username}</h1>
     </>
 }
