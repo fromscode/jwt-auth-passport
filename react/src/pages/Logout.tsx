b87c842b68bcafbd07cd318103a8ba8b0be5bc46
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-04-05 10:16:38 +0000

    Implement logout

--- react/src/pages/Logout.tsx
+++ react/src/pages/Logout.tsx
@@ -1,15 +1,30 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react";;
 
 export default function Logout() {
-  // useEffect(async () => {
-  //   try {
-  //     fetch('loca')
-  //   }
-    
-  // })
+  const [data, setData] = useState<null | string>(null);
 
-  // TODO: implement logout
+  useEffect(() => {
+    async function logout() {
+      try {
+        const response = await fetch('http://localhost:3000/logout');
 
+        if (response.status == 200) {
+          localStorage.removeItem('token');
+          setData('Logout successfull')
+        }
+        else setData((await response.json()).message);
+      }
+      catch (err) {
+        console.error(err);
+      }
+    }
 
-  return <></>
+    logout();
+  }, [])
+
+  if (!data) return <>Logging out...</>
+
+  return <>
+    {data}
+  </>
 }
